0216186753935e1a8c4c95d90fab81a4937cf62e
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-12-24 18:13:55 +0300

    bug fix

--- src/components/Routes/PrivateRoutes.jsx
+++ src/components/Routes/PrivateRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router";
 
-export default function PrivateRoute() {
+export default function PrivateRoutes() {
   const { isLoggedIn } = useSelector((state) => state.auth);
   if (!isLoggedIn) {
     return <Navigate to="/login" replace={true} />;
