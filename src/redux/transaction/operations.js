db2487f409b809a234c009593a921c65fb82ea27
Mustafa_Tutus <tutusfullstack@gmail.com>
2024-12-24 18:06:36 +0300

    transaction thunks and slice done

---
+++ src/redux/transaction/operations.js
@@ -0,0 +1,53 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { costumAxiosInstance } from "../api/authAxios";
+
+export const fetchTransactions = createAsyncThunk(
+  "transactions/fetchTransactions",
+  async (_, thunkAPI) => {
+    try {
+      const response = await costumAxiosInstance.get("api/transactions");
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addTransaction = createAsyncThunk(
+  "transactions/addTransaction",
+  async (transactionData, thunkAPI) => {
+    try {
+      const response = await costumAxiosInstance.post(
+        "api/transactions",
+        transactionData
+      );
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteTransaction = createAsyncThunk(
+  "transactions/deleteTransaction",
+  async (transactionId, thunkAPI) => {
+    try {
+      await costumAxiosInstance.delete(`/api/transactions/${transactionId}`);
+      return transactionId;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const editTransaction = createAsyncThunk(
+  "transactions/editTransaction",
+  async({transactionId, updateTransaction}, thunkAPI) => {
+    try{
+        const response = await costumAxiosInstance.patch(`/api/transactions/${transactionId}`, updateTransaction);
+        return response.data
+    }catch(e){
+        return thunkAPI.rejectWithValue(e.message)
+    }
+  }
+);
