d5cbb8c48c908032a1d1899e3a8b5d78adbff4ee
pınar <pinar.dogans96@gmail.com>
2024-12-25 21:13:03 +0300

    header section fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import AppRoutes from "./components/Routes/AppRoutes";
+import Header from "./components/Header/index";
 
 
 function App() {
   return (
     <>
      
-      <AppRoutes/>
+      <AppRoutes />
+      <Header/>
     </>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,42 +1,35 @@
 import React from 'react';
-import './Header.module.css';
+import './Header.css';
 import { IoLogOutOutline } from "react-icons/io5";
 import logo from '../../assets/images/MoneyGuardLogo.png';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout } from '../../redux/auth/operations';
 
-
-
-
-
- const Header = () => {
+const Header = () => {
   const dispatch = useDispatch();
   const email = useSelector((state) => state.auth.email);
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-
-  const username = email.split('@')[0]; // Kullanıcı adını email'den al
+  const username = email ? email.split('@')[0] : 'Name'; // Kullanıcı adını email'den al veya 'Guest' göster
 
   const handleClick = () => {
-    dispatch(logout()); // Çıkış işlemi
+    dispatch(logout()); // Redux logout işlemini tetikleme
   };
 
   return (
     <header className="header">
       <div className="header__logo">
         <img src={logo} alt="Logo" className="header__logo-image" />
-        <span className="header__logo-text">Money Guard</span>
       </div>
       <div className="header__user-section">
         <span className="header__username">{username}</span>
-        <button className="header__exit-button" onClick={handleClick}>
-          <IoLogOutOutline />
-          Exit
-        </button>
+        <IoLogOutOutline
+          className="header__exit-icon"
+          onClick={handleClick}
+          style={{ cursor: 'pointer', fontSize: '1.5rem' }} // İkon tıklanabilir ve görsel olarak belirgin olsun
+          title="Exit" // Tooltip için
+        />
       </div>
     </header>
   );
 };
-
-
 
 export default Header;
