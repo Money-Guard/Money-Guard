cdd0ba759252b2fb1388a0e98bc2c6294553ca2b
pınar <pinar.dogans96@gmail.com>
2024-12-25 17:55:33 +0300

    header first draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import AppRoutes from "./components/Routes/AppRoutes";
 
+
 function App() {
   return (
     <>
-      <AppRoutes />
+     
+      <AppRoutes/>
     </>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from 'react';
+import './Header.module.css';
+import { IoLogOutOutline } from "react-icons/io5";
+import logo from '../../assets/images/MoneyGuardLogo.png';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../redux/auth/operations';
 
-export default function Header() {
+
+
+
+
+ const Header = () => {
+  const dispatch = useDispatch();
+  const email = useSelector((state) => state.auth.email);
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  const username = email.split('@')[0]; // Kullanıcı adını email'den al
+
+  const handleClick = () => {
+    dispatch(logOut()); // Çıkış işlemi
+  };
+
   return (
-    <div>index</div>
-  )
-}
+    <header className="header">
+      <div className="header__logo">
+        <img src={logo} alt="Logo" className="header__logo-image" />
+        <span className="header__logo-text">Money Guard</span>
+      </div>
+      <div className="header__user-section">
+        <span className="header__username">{username}</span>
+        <button className="header__exit-button" onClick={handleClick}>
+          <IoLogOutOutline />
+          Exit
+        </button>
+      </div>
+    </header>
+  );
+};
+
+
+
+export default Header;
