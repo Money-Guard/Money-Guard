ba1bb048b71c52b0ead9a66fe0d54f2a5e6814ba
pınar <pinar.dogans96@gmail.com>
2024-12-26 15:24:39 +0300

    header username fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Header.css';
+import styles from './Header.module.css'; 
 import { IoLogOutOutline } from "react-icons/io5";
 import logo from '../../assets/images/MoneyGuardLogo.png';
 import { useDispatch, useSelector } from 'react-redux';
@@ -7,22 +7,21 @@ import { logout } from '../../redux/auth/operations';
 
 const Header = () => {
   const dispatch = useDispatch();
-  const email = useSelector((state) => state.auth.email);
-  const username = email ? email.split('@')[0] : 'Name'; // Kullanıcı adını email'den al veya 'Guest' göster
+  const username = useSelector((state) => state.auth.username); // Username doğrudan alınıyor
 
   const handleClick = () => {
     dispatch(logout()); // Redux logout işlemini tetikleme
   };
 
   return (
-    <header className="header">
-      <div className="header__logo">
-        <img src={logo} alt="Logo" className="header__logo-image" />
+    <header className={styles.Header}>
+      <div className={styles.HeaderLogo}>
+        <img src={logo} alt="Logo" className={styles.HeaderImage} />
       </div>
-      <div className="header__user-section">
-        <span className="header__username">{username}</span>
+      <div className={styles.HeaderUserSection}>
+        <span className={styles.HeaderUserName}>{username || 'Name'}</span> {/* Kullanıcı adı yoksa 'Name' göster */}
         <IoLogOutOutline
-          className="header__exit-icon"
+          className={styles.HeaderExitIcon}
           onClick={handleClick}
           style={{ cursor: 'pointer', fontSize: '1.5rem' }} // İkon tıklanabilir ve görsel olarak belirgin olsun
           title="Exit" // Tooltip için
