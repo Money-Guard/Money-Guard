110ef11a437d8e513287ac21baf2186fba7af223
pınar <pinar.dogans96@gmail.com>
2024-12-28 21:51:12 +0300

    balance css is ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import AppRoutes from "./components/Routes/AppRoutes";
+import Balance from "./components/Sidebar/Balance";
 
 
 
@@ -8,6 +9,7 @@ function App() {
     <>
      
       <AppRoutes />
+      <Balance />
      
     </>
   );
